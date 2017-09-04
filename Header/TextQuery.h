881efdd7dc3ca37e778c3a41cca98c49373d5b4e
lzw <leeashe@outlook.com>
2017-09-05 00:06:08 +0800

    Add files via upload

---
+++ Header/TextQuery.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef TEXTQUERY_H
+#define TEXTQUERY_H
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <memory>
+#include <map>
+#include <set>
+#include <string>
+
+#include "QueryResult.h"
+
+class QueryResult;
+
+class TextQuery
+{
+public:
+	using line_no = std::vector<std::string>::size_type;
+	TextQuery(std::ifstream&);
+	QueryResult query(const std::string&) const;
+	void display_map();
+
+private:
+	std::shared_ptr<std::vector<std::string>> file;
+	std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;
+	
+	static std::string cleanup_str(const std::string&);
+};
+
+#endif // !TEXTQUERY_H
